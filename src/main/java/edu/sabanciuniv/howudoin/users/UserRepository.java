e076279cb2d090954ab550603541280276ba6715
nusret <ahmetnusretavci34@gmail.com>
2024-11-16 23:20:02 +0300

    UserRepository class created

--- src/main/java/edu/sabanciuniv/howudoin/users/UserRepository.java
+++ src/main/java/edu/sabanciuniv/howudoin/users/UserRepository.java
@@ -11,32 +11,24 @@ public interface UserRepository extends MongoRepository<UserModel, String>
 
     /**
      * Find a user by their email address.
-     *
-     * @param email The email of the user.
-     * @return The UserModel if found, otherwise null.
+     * Useful for user registration
      */
     UserModel findByEmail(String email);
 
     /**
      * Find all users with a verified email.
-     *
-     * @return A list of users whose emailVerified field is true.
      */
     List<UserModel> findByEmailVerifiedTrue();
 
     /**
      * Find all users with a specific first name.
-     *
-     * @param firstName The first name to search for.
-     * @return A list of users with the given first name.
+     * Useful for friend requests
      */
     List<UserModel> findByFirstName(String firstName);
 
     /**
      * Find all users whose last name contains a specific substring (case-insensitive).
-     *
-     * @param lastName The substring to search for in the last name.
-     * @return A list of users with matching last names.
+     * Useful for friend requests
      */
     List<UserModel> findByLastNameContainingIgnoreCase(String lastName);
 }
