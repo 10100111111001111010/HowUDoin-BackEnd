aa816175cbdf4a3611c2ad178397fd4fe1f4ab71
nusret <ahmetnusretavci34@gmail.com>
2024-11-17 17:59:54 +0300

    Updated the project properly so that 'Postman' works properly

--- src/main/java/edu/sabanciuniv/howudoin/HowudoinApplication.java
+++ src/main/java/edu/sabanciuniv/howudoin/HowudoinApplication.java
@@ -2,13 +2,13 @@ package edu.sabanciuniv.howudoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class HowudoinApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HowudoinApplication.class, args);
         System.out.println("Howudoin Application Started");
-
     }
-};
+}
